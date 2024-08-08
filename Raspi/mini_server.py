c85e9b6d81aea196b0d8a87af150704b50954498
marcoacolla <marcocolla29@gmail.com>
2024-08-08 16:24:25 -0300

    changing seasons

--- Raspi/mini_server.py
+++ Raspi/mini_server.py
@@ -12,13 +12,13 @@ def on_message(client, userdata, message):
     
 
 def main():
-    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
 
-    mqtt_client.connect('localhost', 1883, 60) 
+    mqtt_client.connect('127.0.0.1', 1883) 
     # Connect to the MQTT server and process messages in a background thread. 
-    mqtt_client.loop_start()
+    mqtt_client.loop_forever()
 
 if __name__ == '__main__':
     print('MQTT to InfluxDB bridge')
