52329b603cf6062df281cb16e290e0973eb2e52b
marcoacolla <marcocolla29@gmail.com>
2024-08-09 12:54:18 -0300

    quick changes

--- Raspi/mini_server.py
+++ Raspi/mini_server.py
@@ -1,25 +1,23 @@
 import paho.mqtt.client as mqtt
 
-def on_connect(self,client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     client.subscribe("/esp8266/temperature")
-    client.subscribe("/esp8266/filhadapulta")
+    client.subscribe("/esp8266/humidity")
 
 def on_message(client, userdata, message):
     print("Received message '" + str(message.payload) + "' on topic '" + message.topic)
     
 
-def main():
-    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
+print('MQTT to InfluxDB bridge')
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
 
-    mqtt_client.connect("mqtt.eclipseprojects.io")
-    # C_onnect to the MQTT server and process messages in a background thread. 
-    mqtt_client.loop_forever()
+mqtt_client.connect("127.0.0.1")
+# C_onnect to the MQTT server and process messages in a background thread. 
+mqtt_client.loop_forever()
 
-if __name__ == '__main__':
-    print('MQTT to InfluxDB bridge')
-    main()
+
