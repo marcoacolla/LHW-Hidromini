c6c8170655bd6c10e3c3eabd18d2e6a136d3e437
marcoacolla <marcocolla29@gmail.com>
2024-08-09 12:31:17 -0300

    getting started code test

--- Raspi/mini_server.py
+++ Raspi/mini_server.py
@@ -1,27 +1,24 @@
 import paho.mqtt.client as mqtt
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
-    client.subscribe("/esp8266/temperature")
-    client.subscribe("/esp8266/filhadapulta")
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
 
-def on_message(client, userdata, message):
-    print("Received message '" + str(message.payload) + "' on topic '" + message.topic)
-    
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
 
-def main():
-    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
 
-    mqtt_client.connect('127.0.0.1', 1883) 
-    # Connect to the MQTT server and process messages in a background thread. 
-    mqtt_client.loop_start()
-    while True :
-        k = 0
+mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
 
-if __name__ == '__main__':
-    print('MQTT to InfluxDB bridge')
-    main()
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+mqttc.loop_forever()
