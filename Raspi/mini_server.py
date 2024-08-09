5b9acd65148f639962de117e40ef6bc3c0256ece
marcoacolla <marcocolla29@gmail.com>
2024-08-09 12:46:07 -0300

    adding self to on_connect()

--- Raspi/mini_server.py
+++ Raspi/mini_server.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(self,client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
