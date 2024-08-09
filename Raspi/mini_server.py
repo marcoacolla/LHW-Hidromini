d9d8b27a24702e231af29a2efc01de7800928cc6
marcoacolla <marcocolla29@gmail.com>
2024-08-09 12:41:25 -0300

    return test

--- Raspi/mini_server.py
+++ Raspi/mini_server.py
@@ -1,44 +1,25 @@
 import paho.mqtt.client as mqtt
 
-def on_subscribe(client, userdata, mid, reason_code_list, properties):
-    # Since we subscribed only for a single channel, reason_code_list contains
-    # a single entry
-    if reason_code_list[0].is_failure:
-        print(f"Broker rejected you subscription: {reason_code_list[0]}")
-    else:
-        print(f"Broker granted the following QoS: {reason_code_list[0].value}")
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
 
-def on_unsubscribe(client, userdata, mid, reason_code_list, properties):
-    # Be careful, the reason_code_list is only present in MQTTv5.
-    # In MQTTv3 it will always be empty
-    if len(reason_code_list) == 0 or not reason_code_list[0].is_failure:
-        print("unsubscribe succeeded (if SUBACK is received in MQTTv3 it success)")
-    else:
-        print(f"Broker replied with failure: {reason_code_list[0]}")
-    client.disconnect()
+    # Subscribing in on_connect() means that if we lose the connection and
+    client.subscribe("/esp8266/temperature")
+    client.subscribe("/esp8266/filhadapulta")
 
 def on_message(client, userdata, message):
-    # userdata is the structure we choose to provide, here it's a list()
-    userdata.append(message.payload)
-    # We only want to process 10 messages
-    if len(userdata) >= 10:
-        client.unsubscribe("$SYS/#")
+    print("Received message '" + str(message.payload) + "' on topic '" + message.topic)
+    
 
-def on_connect(client, userdata, flags, reason_code, properties):
-    if reason_code.is_failure:
-        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
-    else:
-        # we should always subscribe from on_connect callback to be sure
-        # our subscribed is persisted across reconnections.
-        client.subscribe("$SYS/#")
+def main():
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-mqttc.on_subscribe = on_subscribe
-mqttc.on_unsubscribe = on_unsubscribe
+    mqtt_client.connect('127.0.0.1', 1883) 
+    # Connect to the MQTT server and process messages in a background thread. 
+    mqtt_client.loop_forever()
 
-mqttc.user_data_set([])
-mqttc.connect("mqtt.eclipseprojects.io")
-mqttc.loop_forever()
-print(f"Received the following message: {mqttc.user_data_get()}")
+if __name__ == '__main__':
+    print('MQTT to InfluxDB bridge')
+    main()
