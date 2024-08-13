1060aed7d544a393c1955728868ea19ab109f4c0
marcoacolla <marcocolla29@gmail.com>
2024-08-13 13:37:16 -0300

    changing pressure1 on the fly

--- Raspi/mini_server.py
+++ Raspi/mini_server.py
@@ -1,4 +1,11 @@
 import paho.mqtt.client as mqtt
+import requests
+
+url = "http://10.0.0.181:3000/api/variables"
+
+data = {
+    "pressure1": 0
+}
 
 def on_connect(client, userdata, flags, reason_code, properties):
     if reason_code.is_failure:
@@ -11,6 +18,11 @@ def on_connect(client, userdata, flags, reason_code, properties):
 def on_message(client, userdata, message):
     print("Received message '" + str(message.payload) + "' on topic '" + message.topic)
     
+    data["pressure1"] = float(message.payload)
+    response = requests.post(url, json=data)
+    print("Status Code:", response.status_code)
+    print("Resposta:", response.json())
+
 
 print('MQTT to InfluxDB bridge')
 mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
@@ -20,5 +32,3 @@ mqtt_client.on_message = on_message
 mqtt_client.connect("10.0.1.119",1883,60)
 # C_onnect to the MQTT server and process messages in a background thread. 
 mqtt_client.loop_forever()
-
-
