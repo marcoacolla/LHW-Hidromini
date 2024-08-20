ea4567830a6b55162bf62b304bf060e4d85b289a
marcoacolla <marcocolla29@gmail.com>
2024-08-20 11:57:33 -0300

    adding cvs writer

---
+++ Raspi/mini_server.py
@@ -0,0 +1,77 @@
+import paho.mqtt.client as mqtt
+import requests
+import time
+
+url = "http://10.0.0.181:3000/api/sensors"
+
+interval = 0.5
+
+measures = {  
+    "pressure1": {"value": 0.0, "type": float, "id": "00"},
+    "pressure2": {"value": 0.0, "type": float, "id": "01"},
+    "pressure3": {"value": 0.0, "type": float, "id": "02"},
+
+    "motor_rpm": {"value": 0.0, "type": float, "id": "03"},
+    "gen_rpm": {"value": 0.0, "type": float, "id": "04"},
+
+    "dc_cur": {"value": 0.0, "type": float, "id": "05"},
+    "dc_volt": {"value": 0.0, "type": float, "id": "06"},
+
+    "valve1": {"value": False, "type": bool, "id": "07"},
+    "valve2": {"value": False, "type": bool, "id": "08"}
+}
+
+
+def saveToFile():
+
+
+def updateSite(data):
+    response = requests.post(url, json=data)
+    print("Status Code:", response.status_code)
+    print("Resposta:", response.json())
+
+def prepare_data(measures):
+    to_send = {}
+    for key, sub_dict in measures.items():
+        to_send[key] = {"value": sub_dict["value"]}
+    return to_send
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code.is_failure:
+        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+    else:
+    # Subscribing in on_connect() means that if we lose the connection and
+        for topic, info in measures.items():
+            client.subscribe(f"/mcc/{topic}")
+
+
+def on_message(client, userdata, message):
+    global lastMessageTime,isPaused, toSend
+    lastMessageTime = time.time()
+    isPaused = False
+    print("Received message '" + str(message.payload) + "' on topic '" + message.topic)
+    for topic, infos in measures.items():
+        if message.topic == f"/mcc/{topic}":
+            infos["value"] = infos["type"](message.payload)
+    toSend = prepare_data(measures)
+
+    
+
+
+print('MQTT to InfluxDB bridge')
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+
+mqtt_client.connect("10.0.1.119",1883,60)
+# C_onnect to the MQTT server and process messages in a background thread. 
+mqtt_client.loop_forever()
+
+
+
+while True:
+    if (time.time() - lastMessageTime) > interval and isPaused == False:
+        updateSite(toSend)
+        print("Passou 5s sem receber nada")
+        isPaused = True
+    time.sleep(0.1)
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path'); // Módulo para lidar com caminhos de diretórios
 const http = require('http');
 const WebSocket = require('ws');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-
+const csvFilePath = path.join(__dirname, 'CSV/sensor_measures.csv');
 
 const port = 3000;
 
@@ -29,6 +31,24 @@ let measures = {
     "valve2": {"value": false}
 };
 
+// Configuração do CSV Writer
+const csvWriter = createCsvWriter({
+    path: csvFilePath,
+    header: [
+        {id: 'timestamp', title: 'Timestamp'},
+        {id: 'pressure1', title: 'Pressure 1'},
+        {id: 'pressure2', title: 'Pressure 2'},
+        {id: 'pressure3', title: 'Pressure 3'},
+        {id: 'motor_rpm', title: 'Motor RPM'},
+        {id: 'gen_rpm', title: 'Generator RPM'},
+        {id: 'dc_cur', title: 'DC Current'},
+        {id: 'dc_volt', title: 'DC Voltage'},
+        {id: 'valve1', title: 'Valve 1'},
+        {id: 'valve2', title: 'Valve 2'}
+    ],
+    append: fs.existsSync(csvFilePath) // Adiciona dados ao final do arquivo existente
+});
+
 function broadcast(data) {
     wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
@@ -36,7 +56,29 @@ function broadcast(data) {
         }
     });
 }
+function writeCSV(data){
+    const timestamp = new Date().toISOString();
+    const reg = {
+        timestamp: timestamp,
+        pressure1: data.pressure1.value,
+        pressure2: data.pressure2.value,
+        pressure3: data.pressure3.value,
+        motor_rpm: data.motor_rpm.value,
+        gen_rpm: data.gen_rpm.value,
+        dc_cur: data.dc_cur.value,
+        dc_volt: data.dc_volt.value,
+        valve1: data.valve1.value,
+        valve2: data.valve2.value
+    };
 
+    csvWriter.writeRecords([reg])
+        .then(() => {
+            console.log('Dados salvos no CSV com sucesso.');
+        })
+        .catch(err => {
+            console.error('Erro ao salvar dados no CSV:', err);
+        });
+}
 // Rota POST para atualizar uma ou mais variáveis
 app.post('/api/sensors', (req, res) => {
     const novosDados = req.body;
@@ -46,6 +88,7 @@ app.post('/api/sensors', (req, res) => {
             measures[key].value = novosDados[key].value;
         }
     }
+    writeCSV(measures);
     broadcast(measures);
 
     res.json(measures);
