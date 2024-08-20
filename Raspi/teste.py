79f3a8afbefe9b910962dd763019f621ba38a499
marcoacolla <marcocolla29@gmail.com>
2024-08-20 11:21:53 -0300

    running timer tests

--- Raspi/teste.py
+++ Raspi/teste.py
@@ -1,23 +1,11 @@
-measures = {  
-    "pressure1": {"value": 0.0, "type": float, "id": "00"},
-    "pressure2": {"value": 0.0, "type": float, "id": "01"},
-    "pressure3": {"value": 0.0, "type": float, "id": "02"},
-    "motor_rpm": {"value": 0.0, "type": float, "id": "03"},
-    "gen_rpm": {"value": 0.0, "type": float, "id": "04"},
-    "dc_cur": {"value": 0.0, "type": float, "id": "05"},
-    "dc_volt": {"value": 0.0, "type": float, "id": "06"},
-    "valve1": {"value": False, "type": bool, "id": "07"},
-    "valve2": {"value": False, "type": bool, "id": "08"}
-}
 
-# Prepara os dados para envio, ignorando 'id' e 'type'
-def preparar_dados_para_envio(measures):
-    dados_para_envio = {}
-    for key, sub_dict in measures.items():
-        dados_para_envio[key] = {"value": sub_dict["value"]}
-    return dados_para_envio
+import time
 
-# Prepara os dados para envio
-dados_para_envio = preparar_dados_para_envio(measures)
+lastMessageTime = time.time()
+isPaused = False
 
-print(dados_para_envio)
+while True:
+    if (time.time() - lastMessageTime) > 0.5 and isPaused == False:
+        print(time.time() - lastMessageTime)
+        isPaused = True
+    time.sleep(0.1)
