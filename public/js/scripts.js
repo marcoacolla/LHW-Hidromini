054488057775273abde8ab80c405ad9d82a7bf14
marcoacolla <marcocolla29@gmail.com>
2024-08-19 22:21:09 -0300

    adapting updateVariables() logic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,17 @@
-const socket = new WebSocket('ws://10.0.0.181:3000');
+const socket = new WebSocket('ws://localhost:3000');
 
 
  // Função para atualizar o valor na página
     function updateVariables(resp_data) {
-        document.getElementById('pressure1').textContent = resp_data.pressure1 + " Bar";
-        document.getElementById('pressure2').textContent = resp_data.pressure2 + " Bar";
-        document.getElementById('pressure3').textContent = resp_data.pressure3 + " Bar";
+        document.getElementById('pressure1').textContent = resp_data.pressure1.value + " Bar";
+        document.getElementById('pressure2').textContent = resp_data.pressure2.value + " Bar";
+        document.getElementById('pressure3').textContent = resp_data.pressure3.value + " Bar";
 
-        document.getElementById('motor_rpm').textContent = resp_data.motor_rpm + " RPM";
-        document.getElementById('gen_rpm').textContent = resp_data.gen_rpm + " RPM";
+        document.getElementById('motor_rpm').textContent = resp_data.motor_rpm.value + " RPM";
+        document.getElementById('gen_rpm').textContent = resp_data.gen_rpm.value + " RPM";
 
-        document.getElementById('dc_cur').textContent = resp_data.dc_cur + " A";
-        document.getElementById('dc_volt').textContent = resp_data.dc_volt + " V";
+        document.getElementById('dc_cur').textContent = resp_data.dc_cur.value + " A";
+        document.getElementById('dc_volt').textContent = resp_data.dc_volt.value + " V";
  }
  function updateValves(resp_data) {
     // Supondo que o número de válvulas seja conhecido ou esteja em resp_data
