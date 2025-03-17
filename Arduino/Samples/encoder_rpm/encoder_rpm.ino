4c977524cee94595bd90a62cfd6da4ce91f1f832
marcoacolla <marcocolla29@gmail.com>
2025-03-17 10:24:32 -0300

    adding working realy test

--- Arduino/Samples/encoder_rpm/encoder_rpm.ino
+++ Arduino/Samples/encoder_rpm/encoder_rpm.ino
@@ -1,9 +1,5 @@
 
 #include <Wire.h>
-#include <hd44780.h>
-#include <hd44780ioClass/hd44780_I2Cexp.h>
-
-hd44780_I2Cexp lcd;
 
 unsigned long interval = 2500;
 unsigned long previousMillis = 0;
--- Arduino/Samples/relay_test/relay_test.ino
+++ Arduino/Samples/relay_test/relay_test.ino
@@ -1,23 +1,39 @@
-#include <Arduino.h>
 
-int relay = 8;
+int relayPin1 = 8;
+int relayPin2 = 9;
+int relayPin3 = 10;
+int relayPin4 = 11;
+
 volatile byte relayState = HIGH;
 
 long lastMsInterval = 0;
-long msInterval = 5000;
+long msInterval = 4000;
+long eachInterval = 1000;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(relay, OUTPUT);
+  pinMode(relayPin1, OUTPUT);
+  pinMode(relayPin2, OUTPUT);
+  pinMode(relayPin3, OUTPUT);
+  pinMode(relayPin4, OUTPUT);
+
 
 }
 
 void loop() {
+
+
   if((millis() - lastMsInterval) > msInterval){
     lastMsInterval = millis();
 
-    digitalWrite(relay, relayState);
-    Serial.println("LUZ: " +  !relayState);
+    digitalWrite(relayPin1, relayState);
+    delay(eachInterval);
+    digitalWrite(relayPin2, relayState);
+    delay(eachInterval);
+    digitalWrite(relayPin3, relayState);
+    delay(eachInterval);
+    digitalWrite(relayPin4, relayState);
+    delay(eachInterval);
     
     relayState = !relayState;
 
